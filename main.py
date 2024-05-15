3b0f564ebaf7fe70ef306cef157adc497aa660db
Geoffrey-Hagberg <gdhagberg@msn.com>
2024-05-15 08:18:14 -0500

    Revision: write report to text file rather than print to terminal

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-# read_file(filepath) opens the passed file, reads contents to a variable,
+# read_file() opens the passed file, reads contents to a variable,
 # and splits those contents into a word list that can be parsed downstream;
 # returns text (string), word_list (list of strings)
 def read_file(filepath):
     with open(filepath) as f:
         text = f.read()
         word_list = text.split()
+    f.close()
     return word_list
 
-# get_distinct_words creates a dictionary with key: each string from a passed
+# get_distinct_words() creates a dictionary with key: each string from a passed
 # list of strings, and value: the count associated with each of those words
 # TODO: this needs to strip punctuation somehow
 def get_distinct_words(word_list):
@@ -20,7 +21,7 @@ def get_distinct_words(word_list):
             distinct_words[lowered_word] = 1
     return distinct_words
 
-# count_totals(word_list, distinct_words) takes a passed list of strings and a passed
+# count_totals() takes a passed list of strings and a passed
 # dictionary of distinct words and counts the totals of each; returns total_word_count
 # (integer) and total_distinct_words (integer)
 def count_totals(word_list, distinct_words):
@@ -30,13 +31,13 @@ def count_totals(word_list, distinct_words):
         total_distinct_words += 1
     return total_word_count, total_distinct_words
 
-# sort_on(dict) defines a sorting method associated with a passed dictionary,
+# sort_on() defines a sorting method associated with a passed dictionary,
 # requires that the passed dictionary include a key labeled "num" on which to sort;
 # returns the value associated with "num" key
 def sort_on(dict):
     return dict["num"]
 
-# get_sorted_distinct_words(distinct words) takes a dictionary of words and counts per
+# get_sorted_distinct_words() takes a dictionary of words and counts per
 # word and converts that into a list where each item is a dictionary with keys
 # "name" associated with each word from the original dictionary and "num"
 # associated with the respective count; then sorts that list of dictionaries using
@@ -51,26 +52,39 @@ def get_sorted_distinct_words(distinct_words):
     sorted_distinct_words.sort(reverse=True, key=sort_on)
     return sorted_distinct_words
 
+# write_report() is passed a filepath where it will write a report, a filepath
+# indicating what text is reported on, and multiple values calculated by the
+# evaluative functions above, including total word count, count of total distinct
+# words, and the sorted list of each distinct word's respective count. It writes
+# a formatted report to a text file and prints to the terminal a notification
+# indicating where that file is.
+def write_report(report_filepath, source_filepath, total_word_count, total_distinct_words, sorted_distinct_words):
+    with open(report_filepath, "w") as f:
+        f.write(f"- - Report of Words in {source_filepath} - -\n")
+        f.write("\n")
+        f.write(f"The text is {total_word_count} words long.\n")
+        f.write(f"The text uses {total_distinct_words} distinct words.\n")
+        f.write("\n")
+        for word in sorted_distinct_words:
+            f.write(f"The word '{word['name']}' occurs {word['num']} times.\n")
+        f.write("\n")
+        f.write("- - End of report - -")
+    f.close()
+    print(f"Report written to {report_filepath}")
+
 # main() starts by defining what file to evaluate, calls a sequence of functions
 # to read, parse, and count words within that file, takes the outputs of those
 # functions and prints them to the terminal in a formatted report
 # TODO: make the filepath user entry in the terminal instead of hardcoded
 # TODO: output to a text file report instead of to terminal
 def main():
-    filepath = "books/frankenstein.txt"
-    word_list = read_file(filepath)
+    source_filepath = "books/frankenstein.txt"
+    report_filepath = "reports/frankenstein_report.txt"
+    word_list = read_file(source_filepath)
     distinct_words = get_distinct_words(word_list)
     sorted_distinct_words = get_sorted_distinct_words(distinct_words)
     total_word_count, total_distinct_words = count_totals(word_list, distinct_words)
     # print the counted stuff
-    print(f"- - Report of Words in {filepath} - -")
-    print()
-    print(f"The text is {total_word_count} words long.")
-    print(f"The text uses {total_distinct_words} distinct words.")
-    print()
-    for word in sorted_distinct_words:
-        print(f"The word '{word['name']}' occurs {word['num']} times.")
-    print()
-    print("- - End of report - -")
+    write_report(report_filepath, source_filepath, total_word_count, total_distinct_words, sorted_distinct_words)
 
 main()
